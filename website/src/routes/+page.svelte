81f7abc1fbd0a3e0340aacb7d67c7b50e60de8d0
Liam Gallagher <ljpgallagher@xtra.co.nz>
2023-09-15 08:44:23 +1200

    Homepage section - Catch a Ride With Ease

--- website/src/routes/+page.svelte
+++ website/src/routes/+page.svelte
@@ -3,6 +3,10 @@
   import Hops from './home/Hops.svelte';
 </script>
 
+<svelte:head>
+  <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+</svelte:head>
+
 <div class="p-[275px] rounded-full bg-[#F4FF9E] absolute left-[-168px] top-[-57px] shadow-circle" />
 <div class="p-[262px] rounded-full bg-[#F0FF73] absolute left-[-156px] top-[-44px] shadow-circle" />
 <div class="p-[250px] rounded-full bg-primary absolute left-[-143px] top-[-32px] shadow-circle" />
@@ -25,7 +29,21 @@
   ]}
 />
 
-<!-- Catch a Ride With Ease goes here -->
+<div class="pb-[450px]" />
+
+<!-- Catch a Ride With Ease -->
+
+<div class="p-[225px] rounded-full bg-[#F0FF73] absolute right-[-50px] top-[730px] shadow-circle" />
+<div class="p-[212px] rounded-full bg-primary absolute right-[-36px] top-[743px] shadow-circle" />
+
+<div class="relative mt-14 ml-auto mr-8 w-72">
+  <h2 class="text-3xl font-medium text-dark text-center [text-wrap:balance]">Catch a Ride With Ease</h2>
+  <div class="pb-8" />
+  <p class="text-lg font-medium text-dark text-center">
+    Welcome to RideHop, your one-stop solution for seamless urban commuting. Discover how we
+    simplify your daily travel.
+  </p>
+</div>
 
 <Hops
   positions={[
