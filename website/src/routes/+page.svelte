748e327db0a07095357e70bc90e06abbcbea178c
Liam Gallagher <ljpgallagher@xtra.co.nz>
2023-09-15 08:23:10 +1200

    Basic hops on homepage

--- website/src/routes/+page.svelte
+++ website/src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import logo from '$lib/assets/logo.png';
+  import Hops from './home/Hops.svelte';
 </script>
 
 <div class="p-[275px] rounded-full bg-[#F4FF9E] absolute left-[-168px] top-[-57px] shadow-circle" />
@@ -15,5 +16,19 @@
 <div class="p-[60px] rounded-full bg-[#F0FF73] absolute left-[-27px] top-[582px] shadow-circle" />
 <div class="p-[50px] rounded-full bg-primary absolute left-[-17px] top-[592px] shadow-circle" />
 
-<!-- temp to add scroll room -->
-<div class="h-[2000px]" />
+<Hops positions={[[219, 470], [251, 532], [260, 600], [250, 665]]} />
+
+<!-- Catch a Ride With Ease goes here -->
+
+<Hops positions={[[27, 1113], [8, 1169], [36, 1224]]} />
+
+<!-- Real-Time Updates goes here -->
+
+<Hops positions={[[120, 1724], [90, 1778], [75, 1841], [90, 1904], [120, 1958]]} />
+
+<!-- Safe and Secure goes here -->
+
+<Hops positions={[[290, 2433], [272, 2500], [232, 2556]]} />
+
+<!-- temp to add scroll room, remove once contents in -->
+<div class="h-[3000px]" />
---
+++ website/src/routes/home/Hops.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { fade } from 'svelte/transition';
+
+  export let positions: [number, number][];
+  let scroll_y = 0;
+</script>
+
+<svelte:window bind:scrollY={scroll_y} />
+
+{#each positions as [x, y]}
+  <div
+    transition:fade
+    class="p-[25px] rounded-full bg-primary absolute shadow-circle"
+    style="left: {x}px; top: {y}px; opacity: {(scroll_y - (y - 500)) / 100};"
+  />
+{/each}
