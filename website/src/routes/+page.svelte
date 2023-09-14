6fad201a0b7416ca06fce28ee35342e35e46e307
Liam Gallagher <ljpgallagher@xtra.co.nz>
2023-09-15 08:24:02 +1200

    format (mb)

--- website/src/routes/+page.svelte
+++ website/src/routes/+page.svelte
@@ -16,19 +16,46 @@
 <div class="p-[60px] rounded-full bg-[#F0FF73] absolute left-[-27px] top-[582px] shadow-circle" />
 <div class="p-[50px] rounded-full bg-primary absolute left-[-17px] top-[592px] shadow-circle" />
 
-<Hops positions={[[219, 470], [251, 532], [260, 600], [250, 665]]} />
+<Hops
+  positions={[
+    [219, 470],
+    [251, 532],
+    [260, 600],
+    [250, 665],
+  ]}
+/>
 
 <!-- Catch a Ride With Ease goes here -->
 
-<Hops positions={[[27, 1113], [8, 1169], [36, 1224]]} />
+<Hops
+  positions={[
+    [27, 1113],
+    [8, 1169],
+    [36, 1224],
+  ]}
+/>
 
 <!-- Real-Time Updates goes here -->
 
-<Hops positions={[[120, 1724], [90, 1778], [75, 1841], [90, 1904], [120, 1958]]} />
+<Hops
+  positions={[
+    [120, 1724],
+    [90, 1778],
+    [75, 1841],
+    [90, 1904],
+    [120, 1958],
+  ]}
+/>
 
 <!-- Safe and Secure goes here -->
 
-<Hops positions={[[290, 2433], [272, 2500], [232, 2556]]} />
+<Hops
+  positions={[
+    [290, 2433],
+    [272, 2500],
+    [232, 2556],
+  ]}
+/>
 
 <!-- temp to add scroll room, remove once contents in -->
 <div class="h-[3000px]" />
