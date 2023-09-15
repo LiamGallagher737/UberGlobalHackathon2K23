7ef29ff8919c57684e77e8851a1b0d578cef5443
Liam Gallagher <ljpgallagher@xtra.co.nz>
2023-09-16 11:20:06 +1200

    Use Drizzle ORM for database (#8)

---
+++ src/lib/db/schema.ts
@@ -0,0 +1,8 @@
+import { char, integer, pgTable, text } from 'drizzle-orm/pg-core';
+
+export const users = pgTable('user', {
+    email: text('email').notNull().primaryKey().unique(),
+    name: text('name').notNull(),
+    points: integer('points').default(0).notNull(),
+    code: char('code', { length: 8 }).notNull().unique(),
+});
