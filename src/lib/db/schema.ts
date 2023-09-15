a6b2771cbc883ecd5397b34651e6ddb8456b29c2
Liam Gallagher <ljpgallagher@xtra.co.nz>
2023-09-16 11:16:57 +1200

    finailize

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -1,6 +1,8 @@
-import { integer, pgTable, serial } from "drizzle-orm/pg-core";
+import { char, integer, pgTable, text } from "drizzle-orm/pg-core";
 
-export const PageInsights = pgTable("page_insights", {
-    id: serial("id").notNull(),
-    views: integer("views").notNull(),
+export const users = pgTable("user", {
+    email: text("email").notNull().primaryKey().unique(),
+    name: text("name").notNull(),
+    points: integer("points").default(0).notNull(),
+    code: char("code", { length: 8 }).notNull().unique(),
 });
