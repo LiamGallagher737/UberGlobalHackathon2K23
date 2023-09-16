c445946847ce4b744f5e843ccdf97b73948fd72e
Liam Gallagher <ljpgallagher@xtra.co.nz>
2023-09-16 13:15:23 +1200

    fmt

--- src/lib/db/conn.server.ts
+++ src/lib/db/conn.server.ts
@@ -2,5 +2,5 @@ import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import { PRIVATE_DATEBASE_URL } from '$env/static/private';
 
-const client = postgres(PRIVATE_DATEBASE_URL)
+const client = postgres(PRIVATE_DATEBASE_URL);
 export const conn = drizzle(client);
