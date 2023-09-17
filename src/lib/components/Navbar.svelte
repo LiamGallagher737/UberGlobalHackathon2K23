5f97ac1ed865d65ae5deb5eba8ca5b68e4723fd3
David <62733874+David35k@users.noreply.github.com>
2023-09-17 22:46:54 +1200

    Homepage (#36)

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,45 +1,35 @@
 <script lang="ts">
-  import logo from '$lib/assets/logo.svg';
-
   let navOpen: boolean = false;
 
   function toggleNav() {
     navOpen = !navOpen;
   }
-
-  const navLinks = [
-    { title: 'Home', route: '/' },
-    { title: 'Ride', route: '/ride' },
-    { title: 'Leaderboards', route: '/leaderboards' },
-    { title: 'About', route: '/about' },
-  ];
 </script>
 
 <header class="z-10 h-20 w-[100vw] fixed top-0 left-0">
-  <nav class="flex items-center justify-between h-full mx-5">
-    <span class=""><img class="t h-10" src={logo} alt="" /></span>
-
+  <nav class="flex items-center h-full mx-5 justify-end">
     <button
       on:click={toggleNav}
       id="burger"
       class="t z-20 h-8 w-10 flex flex-col items-end justify-between"
       class:clicked={navOpen}
     >
-      <div class="transition duration-300 burger-line w-10 h-[2px] rounded-full bg-accent" />
-      <div class="transition duration-300 burger-line w-6 h-[2px] rounded-full bg-accent" />
-      <div class="transition duration-300 burger-line w-9 h-[2px] rounded-full bg-accent" />
+      <div class="transition duration-300 burger-line w-10 h-[2px] rounded-full grad" />
+      <div class="transition duration-300 burger-line w-6 h-[2px] rounded-full grad" />
+      <div class="transition duration-300 burger-line w-9 h-[2px] rounded-full grad" />
     </button>
 
     <div
-      class="transition duration-700 z-10 w-[100vw] h-[100vh] backdrop-blur-lg absolute top-0 left-[-100vw] flex items-center justify-evenly"
+      class="transition duration-700 z-10 w-[100vw] h-[100vh] bg-grad absolute top-0 left-[-100vw] flex items-center justify-evenly"
       class:active={navOpen}
     >
-      <ul
-        class="text-accent font-thin text-4xl tracking-[4px] flex flex-col items-center gap-8 align-middle"
-      >
-        {#each navLinks as { title, route }}
-          <li><a href={route} on:click={() => (navOpen = false)}>{title}</a></li>
-        {/each}
+      <ul class="text-white font-black flex flex-col items-center gap-2 align-middle">
+        <li><a on:click={toggleNav} href="/">HOME</a></li>
+        <li><a on:click={toggleNav} href="/ride">RIDE</a></li>
+        <li><a on:click={toggleNav} href="/leaderboard">LEADERBOARD</a></li>
+        <li><a on:click={toggleNav} href="/raffles">RAFFLES</a></li>
+        <li><a on:click={toggleNav} href="/about">ABOUT</a></li>
+        <li><a on:click={toggleNav} href="/contact">CONTACT</a></li>
       </ul>
     </div>
   </nav>
@@ -61,9 +51,24 @@
 
   .clicked div:nth-child(1) {
     transform: translateY(1rem) translateY(-50%) rotate(-45deg);
+    background: white;
   }
 
   .clicked div:nth-child(3) {
     transform: translateY(-1rem) translateY(50%) rotate(45deg);
+    background: white;
+  }
+
+  .bg-grad {
+    background-image: linear-gradient(to bottom right, #f3ed47, #2adc7d);
+  }
+
+  .grad {
+    background-image: linear-gradient(45deg, #f3ed47, #2adc7d);
+  }
+
+  li {
+    text-shadow: 0px 2px 4px rgba(0, 0, 0, 0.25);
+    font-size: 2.7rem;
   }
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,159 @@
 <script lang="ts">
-  import circles from '$lib/assets/cccircular.png';
+  import logo from '$lib/assets/logo.svg';
+  import blob from '$lib/assets/blobs/blob.svg';
+  import blob2 from '$lib/assets/blobs/blob2.svg';
+  import blob3 from '$lib/assets/blobs/blob3.svg';
+  import blob4 from '$lib/assets/blobs/blob4.svg';
 </script>
 
-<section id="sec-1" class="t w-[100vw] h-[100vh] bg-white">
-  <div id="text-wrapper" class="t text-4xl font-thin w-9/12 absolute top-1/3 left-6">
-    <h1>Helping the environment.</h1>
-    <h1>One journey at a time.</h1>
+<section id="sec-1" class="w-[100vw] h-[100vh] bg-white flex justify-center">
+  <div id="text-wrapper" class="font-thin w-9/12 absolute top-1/4">
+    <span class=""><img class="h-15" src={logo} alt="logo" /></span>
+    <h1 class="text-center gradient-text font-semibold text-2xl pt-5">
+      Unlocking a greener future
+    </h1>
   </div>
-  <img class="t absolute bottom-0" src={circles} alt="" />
+</section>
+
+<section id="sec-2" class="w-[100vw] h-[120vh] bg-white">
+  <div class="blob -mr-20 text-center top-20">
+    <img class="scale-150 drop-shadow-xl" src={blob} alt="blob" />
+    <div class="absolute top-5 left-20 w-44 flex flex-col items-center">
+      <h1 class="blob-title">Our Vision for a Greener Tomorrow</h1>
+      <p class="blob-text w-64">
+        Empowering eco-conscious individuals to make a positive impact on our planet through
+        sustainable ridesharing experiences, while fostering a sense of community and healthy
+        competition among users, all in pursuit of a greener, more sustainable future for
+        generations to come.
+      </p>
+    </div>
+  </div>
+
+  <div class="blob text-center top-44">
+    <img class="scale-150 drop-shadow-xl" src={blob2} alt="blob" />
+    <div class="absolute top-6 left-24 w-56 flex flex-col items-center">
+      <h1 class="blob-title">Join the Ecoride Community</h1>
+      <p class="blob-text w-96">
+        Ecoride is more than just a rideshare platform; it's a thriving community of like-minded
+        individuals dedicated to eco-conscious transportation. With our Eco Score system, you can
+        actively engage, compete, and collaborate with fellow users to create a greener future. Your
+        rides with Ecoride go beyond commuting; they contribute to a collective mission. Join the
+        Ecoride community today and be a part of the change.
+      </p>
+    </div>
+  </div>
+</section>
+
+<section id="sec-3" class="w-[100vw] h-[100vh] bg-white">
+  <div class="blob -mr-5 text-center -top-16">
+    <img class="drop-shadow-xl" src={blob4} alt="blob" />
+    <div class="absolute top-32 left-32 w-44 flex flex-col items-center">
+      <h1 class="blob-title">Start Riding Eco-Friendly</h1>
+
+      <div class="bg-white rounded-2xl mt-5">
+        <button class="sign-up-button gradient-text rounded-2xl"> Sign Up </button>
+      </div>
+    </div>
+  </div>
+
+  <div class="blob text-center top-30 -mr-10">
+    <img class="scale-150 drop-shadow-xl" src={blob3} alt="blob" />
+    <div class="absolute top-12 left-16 w-56 flex flex-col items-center">
+      <h1 class="blob-title">Driving Innovation for Sustainability</h1>
+      <p class="blob-text w-96">
+        Our Vision for a Greener Tomorrow Empowering eco-conscious individuals to make a positive
+        impact on our planet through sustainable ridesharing experiences, while fostering a sense of
+        community and healthy competition among users, all in pursuit of a greener, more sustainable
+        future for generations to come.
+      </p>
+    </div>
+  </div>
+</section>
+
+<section id="sec-4" class="w-[100vw] h-[100vh] bg-white">
+  <div class="blob text-center top-20 -ml-10">
+    <img class="scale-[1.75] drop-shadow-xl" src={blob2} alt="blob" />
+    <div class="absolute top-1 left-20 w-56 flex flex-col items-center">
+      <h1 class="blob-title">Climb the Leaderboards with Your Eco Score</h1>
+      <p class="blob-text w-96">
+        Our Eco Score system tracks and rewards your eco-friendly ridesharing choices, allowing you
+        to earn a coveted spot on our leaderboards. Climb to the top by choosing shared rides,
+        opting for electric vehicles, and making eco-conscious decisions. Your journey towards a
+        higher Eco Score not only reflects your dedication but also contributes to a collective
+        effort to reduce carbon emissions. Join the race to the leaderboard and show the world how
+        your rides are making a positive impact on the environment.
+      </p>
+    </div>
+  </div>
+
+  <div class="blob -mr-5 text-center top-24">
+    <img class="drop-shadow-xl" src={blob4} alt="blob" />
+    <div class="absolute top-36 left-20 w-72 flex flex-col items-center">
+      <h1 class="blob-title">Join the Ecoride Movement Today</h1>
+
+      <div class="bg-white rounded-2xl mt-5">
+        <button class="sign-up-button gradient-text rounded-2xl">Sign Up</button>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section id="sec-5" class="w-[100vw] h-[25vh] bg-white">
+  <div class="blob -mr-24 text-center -top-24">
+    <img class="scale-50 drop-shadow-xl" src={blob2} alt="blob" />
+    <div class="absolute top-44 left-16 w-72 flex flex-col items-center">
+      <a class="blob-title" href="#sec-1">Back to top</a>
+    </div>
+  </div>
 </section>
 
 <style>
   :global(body) {
     background-color: rgb(0, 0, 0);
+  }
+
+  .gradient-text {
+    background-image: linear-gradient(45deg, #f3ed47, #2adc7d);
+    background-clip: text;
+    -webkit-background-clip: text;
+    -moz-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    -moz-text-fill-color: transparent;
+  }
+
+  .blob {
+    float: right !important;
+    width: 100%;
+    position: relative;
+    height: auto;
+  }
+
+  .blob-title {
+    color: white;
+    font-size: x-large;
+    font-weight: 900;
+    text-shadow: 0px 2px 4px rgba(0, 0, 0, 0.25);
+    line-height: normal;
+  }
+
+  .blob-text {
+    color: white;
+    text-align: center;
+    font-size: 1.125rem /* 18px */;
+    line-height: 1.75rem /* 28px */;
+    font-style: normal;
+    font-weight: 600;
+    line-height: normal;
+    padding-top: 1rem;
+  }
+
+  .sign-up-button {
+    box-shadow: 2px 2px 4px 0px rgba(0, 0, 0, 0.25);
+    font-size: x-large;
+    padding: 0.8rem 1.5rem;
+  }
+
+  section {
+    overflow: hidden;
   }
 </style>
