6248fcf36a6db29b6d0fbf71e4dbe5f3c90cc579
William L <83631101+wdlea@users.noreply.github.com>
2023-09-20 04:02:36 +0000

    We don't talk about the previous commits

--- src/routes/user/[code]/+page.svelte
+++ src/routes/user/[code]/+page.svelte
@@ -23,7 +23,7 @@
 <div class="mt-16" />
 
 <div class="flex flex-col items-center justify-start">
-  <Profile name={data.name} isPrivate={data.private} />
+  <Profile name={data.name} isPrivate={true} />
 
   {#if data.isFriend}
     <button
