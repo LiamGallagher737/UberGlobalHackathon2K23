3de9856a569951e5982f96bef4516d08ad90b2fe
Liam Gallagher <ljpgallagher@xtra.co.nz>
2023-09-20 23:37:21 +1200

    New ride page (#52)

--- src/routes/ride/+page.server.ts
+++ src/routes/ride/+page.server.ts
@@ -1,6 +1,17 @@
+import { conn } from '$lib/db/conn.server';
+import { users } from '$lib/db/schema';
 import forceLogin from '$lib/forceLogin';
+import { eq } from 'drizzle-orm';
 import type { PageServerLoad } from '../user/$types';
+import { error } from '@sveltejs/kit';
 
 export const load: PageServerLoad = async ({ locals }) => {
-    await forceLogin(locals);
+    const { email } = await forceLogin(locals);
+
+    const res = await conn.select({ cars: users.cars }).from(users).where(eq(users.email, email));
+    if (res.length === 0) throw error(500, 'Failed to get yours cars');
+
+    return {
+        cars: res[0].cars ?? [],
+    };
 };
