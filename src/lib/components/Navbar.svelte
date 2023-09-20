556d1fd32c29166c6de657d0ced41453a4057875
William L <83631101+wdlea@users.noreply.github.com>
2023-09-20 01:04:46 +0000

    Put links to profile in navbar and footer

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -27,6 +27,7 @@
         <li><a on:click={toggleNav} href="/">HOME</a></li>
         <li><a on:click={toggleNav} href="/ride">RIDE</a></li>
         <li><a on:click={toggleNav} href="/leaderboard">LEADERBOARD</a></li>
+        <li><a on:click={toggleNav} href="/user">PROFILE</a></li>
         <li><a on:click={toggleNav} href="/raffles">RAFFLES</a></li>
         <li><a on:click={toggleNav} href="/about">ABOUT</a></li>
         <li><a on:click={toggleNav} href="/contact">CONTACT</a></li>
