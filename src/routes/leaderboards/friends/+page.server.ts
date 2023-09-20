eca92653ab1b843f979227925042430d348eced1
Liam Gallagher <ljpgallagher@xtra.co.nz>
2023-09-21 09:42:13 +1200

    Fix friends leaderboard

--- src/routes/leaderboards/friends/+page.server.ts
+++ src/routes/leaderboards/friends/+page.server.ts
@@ -8,13 +8,18 @@ type LeaderboardEntry = {
 };
 
 type Leaderboard = {
-    result: LeaderboardEntry[];
+    leaderboard: LeaderboardEntry[] | null;
+    user: LeaderboardEntry;
 };
 
 export const load = (async ({ locals, fetch }) => {
     await forceLogin(locals);
     const res = await fetch(`/api/leaderboard/friends`);
     if (!res.ok) throw error(500, 'Failed to fetch leaderboard');
-    const leaderboard: Leaderboard = await res.json();
-    return leaderboard;
+    const result: Leaderboard = await res.json();
+    result.leaderboard?.push({ points: result.user.points, name: 'You' });
+    result.leaderboard?.sort((a, b) => b.points - a.points);
+    return {
+        leaderboard: result.leaderboard,
+    };
 }) satisfies PageServerLoad;
