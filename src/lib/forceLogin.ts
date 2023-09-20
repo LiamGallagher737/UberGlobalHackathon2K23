1caae1191959e7da9c2b8266eeaa887a8f90b102
William L <83631101+wdlea@users.noreply.github.com>
2023-09-20 01:38:58 +0000

    Made forceLogin function and documented it

---
+++ src/lib/forceLogin.ts
@@ -0,0 +1,13 @@
+import { redirect } from "@sveltejs/kit";
+
+/**
+ * Forces the user to have a valid session to continue execution.
+ * If the user does not have a valid session, they are redirected 
+ * to the signin page.
+ * @param locals The app locals, pass directly from the .server.ts arguments 
+ */
+export default async function forceLogin(locals: App.Locals) {
+    const session = await locals.getSession();
+
+    if (!session?.user?.email) throw redirect(302, '/auth');
+}
