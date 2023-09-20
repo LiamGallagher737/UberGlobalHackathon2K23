1c4cd58f192a712d3f10c6fb6fcdb2ecca75e626
David <davidtoman07@gmail.com>
2023-09-20 18:48:08 +1200

    made auth page look nice

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -7,21 +7,34 @@
 </script>
 
 <section class="flex items-center justify-around h-screen bg-white">
-  <div
-    class="w-4/5 h-1/3 md:w-1/3 md:h-1/2 rounded-3xl shadow-lg shadow-green-400 bg-gray-50 flex flex-col items-center justify-between"
-  >
-    {#if $page.data.session}
-      <!-- {#if $page.data.session.user?.image}
-        <span style="background-image: url('{$page.data.session.user.image}')" class="avatar" />
-      {/if} -->
-      <p class="signedInText mb-6 m-10">
-        <small>Signed in as</small><br />
-        <strong>{$page.data.session.user?.name ?? 'User'}</strong>
-      </p>
-    {:else}
-      <p class="notSignedInText text-red-500 my-6">You are not signed in</p>
-    {/if}
-
+  <div class="drop-shadow-xl w-5/6 h-1/2 gradient-border flex flex-col justify-between items-center rounded-3xl">
+    <div class="md:w-1/3 md:h-1/2 text-center">
+      {#if $page.data.session}
+        <p class="signedInText mb-6 m-10">
+          <small>Signed in as</small><br />
+          <strong>{$page.data.session.user?.name ?? 'User'}</strong>
+        </p>
+      {:else}
+        <p class="notSignedInText gradient-text font-bold text-3xl mt-10">You are not signed in</p>
+      {/if}
+    </div>
     <SigninButton {signedIn} on:signIn={signIn} on:signOut={signOut} />
   </div>
 </section>
+
+<style>
+  .gradient-text {
+    background-image: linear-gradient(45deg, #f3ed47, #2adc7d);
+    background-clip: text;
+    -webkit-background-clip: text;
+    -moz-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    -moz-text-fill-color: transparent;
+  }
+
+  .gradient-border {
+    background: linear-gradient(#fff, #fff) padding-box,
+    linear-gradient(135deg, #f3ed47, #2adc7d) border-box;
+    border: 5px solid transparent;
+  }
+</style>
